27b4ebf850ae3d1196534dbed3af09e645a532c0
Jayce Lowry <jaycelowry@u.boisestate.edu>
2026-03-22 17:08:50 -0600

    Update db queries

--- src/main/java/com/heroku/java/GettingStartedApplication.java
+++ src/main/java/com/heroku/java/GettingStartedApplication.java
@@ -10,6 +10,7 @@ import javax.sql.DataSource;
 import java.sql.Connection;
 import java.util.ArrayList;
 import java.util.Map;
+import java.util.UUID;
 
 @SpringBootApplication
 @Controller
@@ -30,13 +31,14 @@ public class GettingStartedApplication {
     String database(Map<String, Object> model) {
         try (Connection connection = dataSource.getConnection()) {
             final var statement = connection.createStatement();
-            statement.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
-            statement.executeUpdate("INSERT INTO ticks VALUES (now())");
+            statement.executeUpdate("CREATE TABLE IF NOT EXISTS table_timestamp_and_random_string (tick timestamp, random_string varchar(50))");
+            statement.executeUpdate("INSERT INTO table_timestamp_and_random_string VALUES (now(), '" + getRandomString() + "')");
 
-            final var resultSet = statement.executeQuery("SELECT tick FROM ticks");
+
+            final var resultSet = statement.executeQuery("SELECT tick, random_string FROM table_timestamp_and_random_string");
             final var output = new ArrayList<>();
             while (resultSet.next()) {
-                output.add("Read from DB: " + resultSet.getTimestamp("tick"));
+                output.add("Read from DB: " + resultSet.getTimestamp("tick") + " " + resultSet.getString("random_string"));
             }
 
             model.put("records", output);
@@ -46,6 +48,10 @@ public class GettingStartedApplication {
             model.put("message", t.getMessage());
             return "error";
         }
+    }
+
+    private String getRandomString() {
+        return UUID.randomUUID().toString().replace("-", "");
     }
 
     public static void main(String[] args) {
